KCUFRom/android_framework_base

Store kcuf fingerprint for package data
and use that to clear package cache instead of our
recovery script solution

Change-Id: I21f599a84f8ff28b53bc088f739374c037f81be0

core/java/android/os/Build.java
@@ -1002,6 +1002,19 @@ public class Build {
     public static final String FINGERPRINT = deriveFingerprint();
 
     /**
+     * KCUF fingerprint
+     * @hide
+     */
+    public static final String PROP_KCUF_FINGERPRINT = "org.kcuf.fingerprint";
+
+    /**
+     * KCUF fingerprint
+     * @hide
+     */
+    public static final String KCUF_FINGERPRINT = SystemProperties.get(PROP_KCUF_FINGERPRINT,
+            deriveFingerprint());
+
+    /**
      * Some devices split the fingerprint components between multiple
      * partitions, so we might derive the fingerprint at runtime.
      */

learningtalabi/android_frameworks_base

core/java/android/os/Build.java
@@ -1002,6 +1002,19 @@ public class Build {
     public static final String FINGERPRINT = deriveFingerprint();
 
     /**
+     * KCUF fingerprint
+     * @hide
+     */
+    public static final String PROP_KCUF_FINGERPRINT = "ro.kcuf.fingerprint";
+
+    /**
+     * KCUF fingerprint
+     * @hide
+     */
+    public static final String KCUF_FINGERPRINT = SystemProperties.get(PROP_KCUF_FINGERPRINT,
+            deriveFingerprint());
+
+    /**
      * Some devices split the fingerprint components between multiple
      * partitions, so we might derive the fingerprint at runtime.
      */