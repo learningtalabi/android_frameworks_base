b3edecfc5d24f2cfeb25eed7f02e0f6d4a315fc1
the orchidproject <aosporchid@gmail.com>
2021-05-05 23:05:52 +0100

    Revert "Battery bar enhancements [1/2]"
    This reverts commit d6eddc9bcbc58775f5678eb8bae867e938829c74.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryBarController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryBarController.java
@@ -27,7 +27,6 @@ public class BatteryBarController extends LinearLayout {
 
     public static final int STYLE_REGULAR = 0;
     public static final int STYLE_SYMMETRIC = 1;
-    public static final int STYLE_REVERSE = 2;
 
     int mStyle = STYLE_REGULAR;
     int mLocation = 0;
@@ -168,11 +167,6 @@ public class BatteryBarController extends LinearLayout {
                         LayoutParams.MATCH_PARENT, 1)));
             }
 
-        } else if (mStyle == STYLE_REVERSE) {
-            BatteryBar bar = new BatteryBar(mContext, mBatteryCharging, mBatteryLevel, isVertical);
-            bar.setRotation(180);
-            addView(bar, new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT,
-                        LayoutParams.MATCH_PARENT, 1));
         }
     }
 
