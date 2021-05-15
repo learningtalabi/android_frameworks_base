a5846e60a06dbcd6aeb9386750c31b745bd00444
Orchid Os Rom <learningtalabi@gmail.com>
2021-05-15 10:57:27 +0100

    reverts sfuny clock

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -159,13 +159,6 @@ public final class ClockManager {
         addBuiltinClock(() -> new LineageClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new FluidClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new TuxClockController(res, layoutInflater, colorExtractor));
-        addBuiltinClock(() -> new SfunyClockController(res, layoutInflater, colorExtractor));
-        addBuiltinClock(() -> new BubbleAnimationClockController(res, layoutInflater, colorExtractor, context));
-        addBuiltinClock(() -> new RainbowCircleClockController(res, layoutInflater, colorExtractor, context));
-        addBuiltinClock(() -> new LoadingClockController(res, layoutInflater, colorExtractor, context));
-	addBuiltinClock(() -> new FireCircleClockController(res, layoutInflater, colorExtractor, context));
-        addBuiltinClock(() -> new SwingClockController(res, layoutInflater, colorExtractor, context));
-
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
         mWidth = dm.widthPixels;
